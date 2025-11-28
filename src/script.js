88a51e6ac794e0603dbaf2848b9ee3084162ab5b
valenis96 <valentina@maronese.com>
2025-11-28 11:14:04 +0100

    Trying express

--- src/script.js
+++ src/script.js
@@ -1,8 +1,9 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.hello = hello;
+import express from 'express';
 const world = 'world';
-function hello(who = world) {
+export function hello(who = world) {
     return `Hello ${who}!`;
 }
 console.log(hello());
+const app = express();
+app.get('/', (req, res) => res.send('Hello from Express + TypeScript!'));
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
--- src/script.ts
+++ src/script.ts
@@ -1,7 +1,12 @@
+import express from 'express';
 const world = 'world';
 
 export function hello(who: string = world): string {
   return `Hello ${who}!`;
-   }
-   console.log(hello());
-   
+}
+
+console.log(hello());
+
+const app = express();
+app.get('/', (req, res) => res.send('Hello from Express + TypeScript!'));
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
